89073813ae010b48d0d95c607bf3145eade2f051
User <User@DESKTOP-OBD5518>
2026-06-07 15:45:07 +0500

    final

---
+++ C++_LABA_8/input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include "input.h"
+int Input_Int() {
+    int number;
+    while (true) {
+        if (std::cin >> number) {
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            return number;
+        }
+        else {
+            std::cout << "Error. Enter number." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
+}
--- C++_LABA_8/main.cpp
+++ C++_LABA_8/main.cpp
@@ -2,12 +2,13 @@
 #include "BackRec5.h"
 #include "HomeDyn3.h"
 #include "HomeDyn15.h"
+#include "input.h"
 int main() {
     setlocale(LC_ALL, "");
     int choice = 0;
     std::string menu_message = "Меню:\n1)BackRec5\n2)HomeDyn3\n3)HomeDyn15\n4)Выход\nВведите номер выбранного пункта меню: ";
     std::cout << menu_message;
-    std::cin >> choice;
+    choice = Input_Int();
     while (choice != 4) {
         if (choice < 1 || choice > 4) {
             std::cout << "Ошибка. Некорректный ввод, попробуйте еще раз: " << std::endl;
